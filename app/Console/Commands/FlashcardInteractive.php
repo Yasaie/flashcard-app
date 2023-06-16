54a160e11491e6d7add23f9f3ce5124f08513e55
Payam Yasaie <payam@owow.io>
2023-06-16 14:47:18 +0330

    create flashcard command

---
+++ app/Console/Commands/FlashcardInteractive.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FlashcardInteractive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'flashcard:interactive';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Interactive CLI program for Flashcard practice';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
